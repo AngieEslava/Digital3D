2de6326ca517d21a95236cf315e77432548d0dfb
AngieEslava <egangiest@gmail.com>
2020-04-23 09:08:26 -0500

    ultimos cambios

---
+++ js/mi-script-.js
@@ -0,0 +1,23 @@
+let num1 = prompt("digite numero 1: ");
+var num2 = prompt("digite numero 2: ");
+const suma=num1+num2;
+
+// num1 = 10
+// suma=num1+num2
+// console.log(4!=4);
+// alert("la suma es: " + suma );
+
+if(num1 >= num2){
+    console.log(num1, "es mayor o igual a", num2, "!!!");
+}else{
+    console.log("la condicion no se cumple!!!");
+}
+
+// ------------ Tarea mayor o menor de edad-----------------
+// let edad = prompt("Digite su edad: ");	                //#
+// if(edad >= 18){                                          //|
+//     console.log("la persona es mayor de edad");          //|
+// }else{                                                   //|
+//     console.log("la persona es menor de edad");          //|
+// }                                                        //|
+
